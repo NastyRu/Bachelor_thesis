8a4db2e86b2fa389f13b56686e151cfde0cdfdbb
NastyRu <nasty.sidenko@mail.ru>
2021-05-21 02:19:25 +0300

    src: Update architecture

--- src/googlenet/inception_v1.py
+++ src/googlenet/inception_v1.py
@@ -1,13 +1,14 @@
 from tensorflow.keras.layers import Convolution2D, MaxPooling2D, Input, \
-                         Concatenate, AveragePooling2D, Dropout, Dense
+                         Concatenate, AveragePooling2D, Dropout, Dense, \
+                         Flatten
 from tensorflow.keras.models import Model
 
-class_num = 11
+CLASS_NUM = 5
 
 
-class inceptionV1():
+class InceptionV1():
     def __init__(self):
-        self.name = "inceptionV1"
+        self.name = "InceptionV1"
 
     def architecture(self):
         input = Input(shape=(224, 224, 3))
@@ -35,14 +36,14 @@ class inceptionV1():
                              strides=2,
                              padding='same')(layer)
 
-        layer = self.inception(input=layer,
+        layer = self.Inception(input=layer,
                                filters_1x1=64,
                                filters_3x3_reduce=96,
                                filters_3x3=128,
                                filters_5x5_reduce=16,
                                filters_5x5=32,
                                filters_pool_proj=32)
-        layer = self.inception(input=layer,
+        layer = self.Inception(input=layer,
                                filters_1x1=128,
                                filters_3x3_reduce=128,
                                filters_3x3=192,
@@ -53,7 +54,7 @@ class inceptionV1():
                              strides=2,
                              padding='same')(layer)
 
-        layer = self.inception(input=layer,
+        layer = self.Inception(input=layer,
                                filters_1x1=192,
                                filters_3x3_reduce=96,
                                filters_3x3=208,
@@ -61,23 +62,23 @@ class inceptionV1():
                                filters_5x5=48,
                                filters_pool_proj=64)
 
-        aux1 = self.auxiliary(layer)
+        aux1 = self.Auxiliary(layer)
 
-        layer = self.inception(input=layer,
+        layer = self.Inception(input=layer,
                                filters_1x1=160,
                                filters_3x3_reduce=112,
                                filters_3x3=224,
                                filters_5x5_reduce=24,
                                filters_5x5=64,
                                filters_pool_proj=64)
-        layer = self.inception(input=layer,
+        layer = self.Inception(input=layer,
                                filters_1x1=128,
                                filters_3x3_reduce=128,
                                filters_3x3=256,
                                filters_5x5_reduce=24,
                                filters_5x5=64,
                                filters_pool_proj=64)
-        layer = self.inception(input=layer,
+        layer = self.Inception(input=layer,
                                filters_1x1=112,
                                filters_3x3_reduce=144,
                                filters_3x3=288,
@@ -85,9 +86,9 @@ class inceptionV1():
                                filters_5x5=64,
                                filters_pool_proj=64)
 
-        aux2 = self.auxiliary(layer)
+        aux2 = self.Auxiliary(layer)
 
-        layer = self.inception(input=layer,
+        layer = self.Inception(input=layer,
                                filters_1x1=256,
                                filters_3x3_reduce=160,
                                filters_3x3=320,
@@ -98,14 +99,14 @@ class inceptionV1():
                              strides=2,
                              padding='same')(layer)
 
-        layer = self.inception(input=layer,
+        layer = self.Inception(input=layer,
                                filters_1x1=256,
                                filters_3x3_reduce=160,
                                filters_3x3=320,
                                filters_5x5_reduce=32,
                                filters_5x5=128,
                                filters_pool_proj=128)
-        layer = self.inception(input=layer,
+        layer = self.Inception(input=layer,
                                filters_1x1=384,
                                filters_3x3_reduce=192,
                                filters_3x3=384,
@@ -114,15 +115,16 @@ class inceptionV1():
                                filters_pool_proj=128)
         layer = AveragePooling2D(pool_size=(7, 7),
                                  strides=1,
-                                 padding='same')(layer)
+                                 padding='valid')(layer)
 
+        layer = Flatten()(layer)
         layer = Dropout(rate=0.4)(layer)
         layer = Dense(units=1000, activation='linear')(layer)
-        output = Dense(units=class_num, activation='softmax')(layer)
+        output = Dense(units=CLASS_NUM, activation='softmax')(layer)
 
         return Model(inputs=input, outputs=[output, aux1, aux2])
 
-    def inception(self, input,
+    def Inception(self, input,
                   filters_1x1,
                   filters_3x3_reduce, filters_3x3,
                   filters_5x5_reduce, filters_5x5,
@@ -164,22 +166,22 @@ class inceptionV1():
                                      padding='same',
                                      activation='relu')(maxpool)
 
-        output = Concatenate(axis=-1)([conv_1x1, conv_3x3,
-                                       conv_5x5, maxpool_proj])
-        return output
+        return Concatenate(axis=-1)([conv_1x1, conv_3x3,
+                                     conv_5x5, maxpool_proj])
 
-    def auxiliary(self, input):
+    def Auxiliary(self, input):
         layer = AveragePooling2D(pool_size=(5, 5),
                                  strides=3,
-                                 padding='same')(input)
+                                 padding='valid')(input)
         layer = Convolution2D(filters=128,
                               kernel_size=(1, 1),
                               strides=1,
                               padding='same',
                               activation='relu')(layer)
+        layer = Flatten()(layer)
         layer = Dense(units=256,
                       activation='relu')(layer)
         layer = Dropout(0.4)(layer)
-        layer = Dense(units=class_num,
+        layer = Dense(units=CLASS_NUM,
                       activation='softmax')(layer)
         return layer
