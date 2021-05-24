8b9c1110b909e2e0a7417bfd8f6028495f453592
NastyRu <nasty.sidenko@mail.ru>
2021-05-24 23:18:17 +0300

    src: Add tf-idf text classification

--- src/text_recognition/text_recognition.py
+++ src/text_recognition/text_recognition.py
@@ -3,10 +3,10 @@ import numpy as np
 import cv2
 
 
-def get_text_from_document(path):
+def get_text_from_document(path, lang):
     image = cv2.imread(path)
 
-    text1 = pytesseract.image_to_string(image, lang='rus')
+    text1 = pytesseract.image_to_string(image, lang=lang)
 
     dilated_img = cv2.dilate(image[:, :, 1], np.ones((7, 7), np.uint8))
     bg_img = cv2.medianBlur(dilated_img, 21)
@@ -14,7 +14,7 @@ def get_text_from_document(path):
     norm_img = cv2.normalize(diff_img, None, alpha=0, beta=255,
                              norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
 
-    text2 = pytesseract.image_to_string(norm_img, lang='rus')
+    text2 = pytesseract.image_to_string(norm_img, lang=lang)
     return text1 + text2
 
 
@@ -31,8 +31,10 @@ def main():
                  "visa_ita": 9,
                  "visa_esp": 10}
 
-    path = ""
-    text = get_text_from_document(path)
+    path = "/Users/anastasia/Desktop/test1.png"
+    text1 = get_text_from_document(path, 'rus')
+    text2 = get_text_from_document(path, 'eng')
+    print(text1 + text2)
 
 
 if __name__ == "__main__":
