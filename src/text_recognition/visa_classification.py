89627c97129e684794dc65f81e14e656258476e7
NastyRu <nasty.sidenko@mail.ru>
2021-05-26 11:42:38 +0300

    src: Correct classification documents by keypoints

--- src/text_recognition/visa_classification.py
+++ src/text_recognition/visa_classification.py
@@ -10,38 +10,20 @@ def bw_image(path):
 
 def get_visa_class(path):
     image = bw_image(path)
-    visa = bw_image("documents_keypoints/visa.png")  # fra
     visum = bw_image("documents_keypoints/visum.png")  # deu
-    visto = bw_image("documents_keypoints/visto.png")  # ita
     visado = bw_image("documents_keypoints/visado.png")  # esp
+    visa = bw_image("documents_keypoints/visa.png")  # fra
+    visto = bw_image("documents_keypoints/visto.png")  # ita
 
-    visto_res = cv2.matchTemplate(image, visto, cv2.TM_CCOEFF_NORMED)
-    visum_res = cv2.matchTemplate(image, visum, cv2.TM_CCOEFF_NORMED)
-    visado_res = cv2.matchTemplate(image, visado, cv2.TM_CCOEFF_NORMED)
-    visa_res = cv2.matchTemplate(image, visa, cv2.TM_CCOEFF_NORMED)
+    _, visto_res, _, _ = cv2.minMaxLoc(cv2.matchTemplate(image, visto,
+                                                         cv2.TM_CCOEFF_NORMED))
+    _, visum_res, _, _ = cv2.minMaxLoc(cv2.matchTemplate(image, visum,
+                                                         cv2.TM_CCOEFF_NORMED))
+    _, visado_res, _, _ = cv2.minMaxLoc(cv2.matchTemplate(image, visado,
+                                                          cv2.TM_CCOEFF_NORMED))
+    _, visa_res, _, _ = cv2.minMaxLoc(cv2.matchTemplate(image, visa,
+                                                        cv2.TM_CCOEFF_NORMED))
 
-    visas = {7: visto_res, 8: visum_res, 9: visado_res, 10: visa_res}
+    visas = {7: visum_res, 8: visado_res, 9: visa_res, 10: visto_res}
 
-    max_key = max(visas, key=visas.get)
-    return max_key
-
-
-def main():
-    documents = {"passport1": 0,
-                 "passport2": 1,
-                 "passport3": 2,
-                 "vu1": 3,
-                 "vu2": 4,
-                 "vu3": 5,
-                 "passport": 6,
-                 "visa_fra": 7,
-                 "vise_deu": 8,
-                 "visa_ita": 9,
-                 "visa_esp": 10}
-
-    path = ""
-    visa_class = get_visa_class(path)
-
-
-if __name__ == "__main__":
-    main()
+    return max(visas, key=visas.get), max(visas.values())
