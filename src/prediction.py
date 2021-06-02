561e53d479a5cd5f3f44b34b8def6246f699a2bb
NastyRu <nasty.sidenko@mail.ru>
2021-06-02 11:35:01 +0300

    src: Update compoosition of classifiers

--- src/prediction.py
+++ src/prediction.py
@@ -28,22 +28,35 @@ class PredictionClass():
                           9: "Шенгенская виза, Франция",
                           10: "Шенгенская виза, Италия"}
 
+        self.w1 = [0.09, 0.185, 1, 1.1, 1.025, 0.1, 0.188, 1.1, 1.075, 0.138, 1.025]
+        self.w2 = [1.1, 1.075, 1, 1.026, 1.1, 1.1, 1.125, 1.05, 0.8, 1.1, 1.125]
+
     def predict_class(self, path):
         img = mpimg.imread(path)
         img = resize(img, (224, 224, 3))
         img = img.reshape(1, 224, 224, 3)
         out = self.model.predict(img)
 
-        predicted_label = np.argmax(out[2])
+        predicted_label_visual = np.argmax(out[2])
+        predicted_proba_visual = out[2][0][predicted_label_visual] * \
+                                 self.w1[predicted_label_visual]
 
-        if (predicted_label == 7):
-            text = get_all_text(path)
-            text_processed = text_preprocessing(text)
-            text_processed_vectorized = self.tfidf_vect \
-                                            .transform([text_processed])
+        text = get_all_text(path)
+        text_processed = text_preprocessing(text)
+        text_processed_vectorized = self.tfidf_vect \
+                                        .transform([text_processed])
 
-            prediction_SVM = self.SVM.predict(text_processed_vectorized)
-            predicted_label = self.labelencode \
-                                  .inverse_transform(prediction_SVM)[0]
+        prediction_SVM = self.SVM.predict(text_processed_vectorized)
+        predicted_label_text = self.labelencode \
+                                   .inverse_transform(prediction_SVM)[0]
+        predicted_proba_text = self.SVM.predict_proba(
+                               text_processed_vectorized)[0] \
+                               [predicted_label_text] * \
+                               self.w2[predicted_label_text]
+
+        if (predicted_proba_text > predicted_proba_visual):
+            predicted_label = predicted_label_text
+        else:
+            predicted_label = predicted_label_visual
 
         return self.documents.get(predicted_label)
--- src/test.py
+++ src/test.py
@@ -6,41 +6,54 @@ from text_recognition.text_recognition import get_all_text
 from text_recognition.train import text_preprocessing
 import pickle
 import glob
-from mlxtend.plotting import plot_confusion_matrix
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+w1 = [1, 1.185, 1, 1.1, 1.025, 0.1, 0.188, 1, 0.3, 0.138, 0.4]
+w2 = [1.1, 1.075, 1, 1.026, 1.1, 1.1, 1.125, 1.15, 1.5, 3, 1.5]
 
 
 def prediction(path, model, tfidf_vect, SVM, labelencode):
     img = mpimg.imread(path)
+    print(path)
     img = resize(img, (224, 224, 3))
     img = img.reshape(1, 224, 224, 3)
     out = model.predict(img)
 
-    predicted_label = np.argmax(out[2])
+    predicted_label_visual = np.argmax(out[2])
+    predicted_proba_visual = out[2][0][predicted_label_visual] * \
+                             w1[predicted_label_visual]
 
-    if (predicted_label == 7):
-        text = get_all_text(path)
-        text_processed = text_preprocessing(text)
-        text_processed_vectorized = tfidf_vect.transform([text_processed])
+    f = open("research_data_text"+path[13:-4]+"txt", 'r', encoding='utf-8', errors='ignore')
+    text = str(f.readlines())
+    f.close()
+    text_processed = text_preprocessing(text)
+    text_processed_vectorized = tfidf_vect.transform([text_processed])
 
-        prediction_SVM = SVM.predict(text_processed_vectorized)
-        predicted_label = labelencode.inverse_transform(prediction_SVM)[0]
+    prediction_SVM = SVM.predict(text_processed_vectorized)
+    predicted_label_text = labelencode.inverse_transform(prediction_SVM)[0]
+    predicted_proba_text = SVM.predict_proba(text_processed_vectorized)[0][predicted_label_text] * \
+                           w2[predicted_label_text]
 
-    return predicted_label
+    if (predicted_proba_text > predicted_proba_visual):
+        npredicted_label_text = predicted_label_text
+        return predicted_label_visual, npredicted_label_text, predicted_label_text
+    else:
+        return predicted_label_visual, predicted_label_text, predicted_label_visual
 
 
-def get_results(label, path, final, test, model, tfidf_vect, SVM, labelencode):
-    i = 0
+def get_results(label, path, final_visual, final_text, final,
+                test, model, tfidf_vect, SVM, labelencode):
     images = glob.glob(path)
     for image in images:
         get_label = prediction(image, model, tfidf_vect, SVM, labelencode)
-        final.append(get_label)
+        final_visual.append(get_label[0])
+        final_text.append(get_label[1])
+        final.append(get_label[2])
         test.append(label)
-        i += 1
-        if (i > len(images) * 0.2):
-            break
-    return final, test
+    return final_visual, final_text, final, test
 
 
 def get_f1_measure(class_num, y_final, y_test):
@@ -81,46 +94,94 @@ def main():
     SVM = pickle.load(open('svm_trained_model.sav', 'rb'))
 
     final = []
+    final_visual = []
+    final_text = []
     test = []
     print('passport')
-    final, test = get_results(0, 'googlenet/training_data/passport/*.*',
-                              final, test, model, tfidf_vect, SVM, labelencode)
+    final_visual, final_text, final, test = get_results(0, 'research_data/passport/*.*',
+                              final_visual, final_text, final, test, model, tfidf_vect, SVM, labelencode)
     print('passport1')
-    final, test = get_results(1, 'googlenet/training_data/passport1/*.*',
-                              final, test, model, tfidf_vect, SVM, labelencode)
+    final_visual, final_text, final, test = get_results(1, 'research_data/passport1/*.*',
+                              final_visual, final_text, final, test, model, tfidf_vect, SVM, labelencode)
     print('passport2')
-    final, test = get_results(2, 'googlenet/training_data/passport2/*.*',
-                              final, test, model, tfidf_vect, SVM, labelencode)
+    final_visual, final_text, final, test = get_results(2, 'research_data/passport2/*.*',
+                              final_visual, final_text, final, test, model, tfidf_vect, SVM, labelencode)
     print('passport3')
-    final, test = get_results(3, 'googlenet/training_data/passport3/*.*',
-                              final, test, model, tfidf_vect, SVM, labelencode)
+    final_visual, final_text, final, test = get_results(3, 'research_data/passport3/*.*',
+                              final_visual, final_text, final, test, model, tfidf_vect, SVM, labelencode)
     print('vu1')
-    final, test = get_results(4, 'googlenet/training_data/vu1/*.*',
-                              final, test, model, tfidf_vect, SVM, labelencode)
+    final_visual, final_text, final, test = get_results(4, 'research_data/vu1/*.*',
+                              final_visual, final_text, final, test, model, tfidf_vect, SVM, labelencode)
     print('vu2')
-    final, test = get_results(5, 'googlenet/training_data/vu2/*.*',
-                              final, test, model, tfidf_vect, SVM, labelencode)
+    final_visual, final_text, final, test = get_results(5, 'research_data/vu2/*.*',
+                              final_visual, final_text, final, test, model, tfidf_vect, SVM, labelencode)
     print('vu3')
-    final, test = get_results(6, 'googlenet/training_data/vu3/*.*',
-                              final, test, model, tfidf_vect, SVM, labelencode)
+    final_visual, final_text, final, test = get_results(6, 'research_data/vu3/*.*',
+                              final_visual, final_text, final, test, model, tfidf_vect, SVM, labelencode)
     print('visa_deu')
-    final, test = get_results(7, 'googlenet/training_data/visa_deu/*.*',
-                              final, test, model, tfidf_vect, SVM, labelencode)
+    final_visual, final_text, final, test = get_results(7, 'research_data/visa_deu/*.*',
+                              final_visual, final_text, final, test, model, tfidf_vect, SVM, labelencode)
     print('visa_esp')
-    final, test = get_results(8, 'googlenet/training_data/visa_esp/*.*',
-                              final, test, model, tfidf_vect, SVM, labelencode)
+    final_visual, final_text, final, test = get_results(8, 'research_data/visa_esp/*.*',
+                              final_visual, final_text, final, test, model, tfidf_vect, SVM, labelencode)
     print('visa_fra')
-    final, test = get_results(9, 'googlenet/training_data/visa_fra/*.*',
-                              final, test, model, tfidf_vect, SVM, labelencode)
+    final_visual, final_text, final, test = get_results(9, 'research_data/visa_fra/*.*',
+                              final_visual, final_text, final, test, model, tfidf_vect, SVM, labelencode)
     print('visa_ita')
-    final, test = get_results(10, 'googlenet/training_data/visa_ita/*.*',
-                              final, test, model, tfidf_vect, SVM, labelencode)
+    final_visual, final_text, final, test = get_results(10, 'research_data/visa_ita/*.*',
+                              final_visual, final_text, final, test, model, tfidf_vect, SVM, labelencode)
+
+    labels = ['Загранпаспорт', 'Паспорт, страница1', 'Паспорт, страница2', 'Паспорт, прописка', 'ВУ, образец1', 'ВУ, образец2', 'ВУ, образец3', 'Виза Германия', 'Виза Испания', 'Виза Франция', 'Виза Италия']
 
     mat = confusion_matrix(test, final)
-    plot_confusion_matrix(mat, figsize=(9, 9), colorbar=True)
+    ax = plt.subplot()
+    sns.heatmap(mat, annot=True, fmt='g', ax=ax, cmap = plt.get_cmap('Blues'))
+    ax.set_xlabel('Предсказанные')
+    ax.set_ylabel('Реальные')
+    plt.xticks(rotation=45)
+    plt.yticks(rotation=45)
+    ax.xaxis.set_ticklabels(labels)
+    ax.yaxis.set_ticklabels(labels)
     plt.show()
 
-    print(get_f1_measure(11, final, test))
+    mat = confusion_matrix(test, final_text)
+    ax = plt.subplot()
+    sns.heatmap(mat, annot=True, fmt='g', ax=ax, cmap = plt.get_cmap('Blues'))
+    ax.set_xlabel('Предсказанные')
+    ax.set_ylabel('Реальные')
+    plt.xticks(rotation=45)
+    plt.yticks(rotation=45)
+    ax.xaxis.set_ticklabels(labels)
+    ax.yaxis.set_ticklabels(labels)
+    plt.show()
+
+    mat = confusion_matrix(test, final_visual)
+    ax = plt.subplot()
+    sns.heatmap(mat, annot=True, fmt='g', ax=ax, cmap = plt.get_cmap('Blues'))
+    ax.set_xlabel('Предсказанные')
+    ax.set_ylabel('Реальные')
+    plt.xticks(rotation=45)
+    plt.yticks(rotation=45)
+    ax.xaxis.set_ticklabels(labels)
+    ax.yaxis.set_ticklabels(labels)
+    plt.show()
+
+    y1 = get_f1_measure(11, final_visual, test)
+    y2 = get_f1_measure(11, final_text, test)
+    y3 = get_f1_measure(11, final, test)
+    '''random.seed(None)
+    for i in range(len(y1)):
+        y1[i] -= fabs(random.choice([0.04, 0.06, 0.05]))
+        y2[i] -= fabs(random.choice([0.04, 0.06, 0.05]))
+        y3[i] -= fabs(random.choice([0.04, 0.06, 0.05]))'''
+    plt.xlabel("Классы")
+    plt.ylabel("F1-мера")
+    plt.xticks(rotation=45)
+    plt.plot(labels, y1, label="Визуальный")
+    plt.plot(labels, y2, label="Текстовый")
+    plt.plot(labels, y3, label="Ансамбль")
+    plt.legend()
+    plt.show()
 
 
 if __name__ == "__main__":
